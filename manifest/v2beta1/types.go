b8582888fe07c7bd1d87c8f8510b0acadaa75fe1
Joao Luna <joaopedroluna98@gmail.com>
2023-01-23 23:54:58 +0000

    Add SslPassthrough to manifest HTTP options

--- manifest/v2beta1/types.go
+++ manifest/v2beta1/types.go
@@ -124,10 +124,11 @@ type ServiceExpose struct {
 }
 
 type ServiceExposeHTTPOptions struct {
-	MaxBodySize uint32
-	ReadTimeout uint32
-	SendTimeout uint32
-	NextTries   uint32
-	NextTimeout uint32
-	NextCases   []string
+	MaxBodySize    uint32
+	ReadTimeout    uint32
+	SendTimeout    uint32
+	NextTries      uint32
+	NextTimeout    uint32
+	NextCases      []string
+	SslPassthrough bool
 }
